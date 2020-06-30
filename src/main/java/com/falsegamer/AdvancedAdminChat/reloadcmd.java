ef43a86cd57df8c036c6b09b78b648a9c7088c68
TheFALSEGamer4 <37382895+TheFALSEGamer4@users.noreply.github.com>
2020-06-29 20:27:09 -0500

    Update reloadcmd.java

--- src/main/java/com/falsegamer/AdvancedAdminChat/reloadcmd.java
+++ src/main/java/com/falsegamer/AdvancedAdminChat/reloadcmd.java
@@ -1,24 +1,30 @@
 package com.falsegamer.AdvancedAdminChat;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-
 
 public class reloadcmd implements CommandExecutor {
 
     private final Main plugin;
+
     public reloadcmd(Main plugin) {
         this.plugin = plugin;
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
         Player player = (Player) sender;
-        if (cmd.getName().equalsIgnoreCase("aoreload")){
-            this.plugin.cm.reload();
-            sender.sendMessage(this.plugin.cm.message(this.plugin.cm.reloadMessage));
+
+        if (cmd.getName().equalsIgnoreCase("aoreload") && sender instanceof Player){
+            if (player.hasPermission("adminonly.reload")) {
+                this.plugin.cm.reload();
+                sender.sendMessage(this.plugin.cm.message(this.plugin.cm.reloadMessage));
+            }else {
+                player.sendMessage(this.plugin.cm.cc(this.plugin.cm.noPermission));
+            }
         }
         return true;
     }
